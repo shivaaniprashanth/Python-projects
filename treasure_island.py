9af771b5214abd7dfec3dcac9761e61a1a3dc898
shivaaniprashanth <84033752+shivaaniprashanth@users.noreply.github.com>
2021-08-23 19:09:24 +0530

    Create treasure_island.py

---
+++ treasure_island.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+direction = input("Would you like to go left or right? \n")
+direction_small = direction.lower()
+if direction_small == "right":
+  print("You have fallen into a hole. \nGAME OVER.")
+else: 
+  swim_wait = input("Would you like to swim or wait? \n")
+  swim_wait_lower = swim_wait.lower()
+  if swim_wait_lower == "swim":
+    print("You have been attacked by a trout. \nGAME OVER.")
+  
+  else:
+    door = input("Which colour door would you like to enter? \n")
+    door_lower = door.lower()
+    if door_lower == "red":
+      print("Burned by fire. \nGAME OVER.")
+    elif door_lower == "yellow":
+      print("Congratulations, YOU WIN.")
+    elif door_lower == "blue":
+      print("Eaten by beasts. \nGAME OVER.")
+    else:
+      print("GAME OVER")
